7637928b66c6d2e8e42778c408096f6ecf6965ce
basgoncalves <basilio.goncalves7@gmail.com>
2025-09-03 16:53:12 +0200

    add plot compare with MRI (thicker lines)

--- code/calculate_muscle_moments.py
+++ code/calculate_muscle_moments.py
@@ -46,7 +46,8 @@ def main(muscleAnalysis_dir, muscleForces_filepath):
                 df = pd.DataFrame(muscle_moments)
                 # add time 
                 df['time'] = muscleForces['time']
-                output_file_path = os.path.join(root, f"{MUSCLE_MOMENTS_PREFIX}{filename}")
+                dof = filename.replace(MUSCLE_ANALYSIS_PREFIX, '').replace('.sto', '')
+                output_file_path = os.path.join(root, f"{MUSCLE_MOMENTS_PREFIX}{dof}.sto")
                 utils.write_sto_file(df, output_file_path)
 
                 print(f"Calculated muscle moments saved in {output_file_path}")
@@ -93,14 +94,18 @@ def groups_muscles(dataFrame, muscleMapping, type ='sum'):
 
     return grouped, muscleMapping_dof
 
-def plot(muscleAnalysis_dir, jointMomentsPath, muscleMapping):
-    import matplotlib.pyplot as plt
-
+def find_muscle_moment_files(muscleAnalysis_dir):
     muscle_moments_files = []
     for root, dirs, files in os.walk(muscleAnalysis_dir):
         for filename in files:
             if filename.startswith(MUSCLE_MOMENTS_PREFIX):
                 muscle_moments_files.append(os.path.join(root, filename))
+    return muscle_moments_files
+
+def plot(muscleAnalysis_dir, jointMomentsPath, muscleMapping):
+    import matplotlib.pyplot as plt
+
+    muscle_moments_files = find_muscle_moment_files(muscleAnalysis_dir)
 
     if not muscle_moments_files:
         print("No muscle moments files found")
@@ -165,26 +170,133 @@ def plot(muscleAnalysis_dir, jointMomentsPath, muscleMapping):
         print(f"Saved plot for file: {output_filepath}")
         plt.close()
 
+def plot_2_trials(muscleAnalysis_dir1, jointMomentsPath1, 
+                  muscleAnalysis_dir2, jointMomentsPath2, 
+                  muscleMapping,
+                  savePath):
+
+    muscle_moments_files1 = find_muscle_moment_files(muscleAnalysis_dir1)
+    muscle_moments_files2 = find_muscle_moment_files(muscleAnalysis_dir2)
+    
+    if not muscle_moments_files1:
+        print(f'Muscle moments files found in {muscleAnalysis_dir1}: {muscle_moments_files1}')
+        return
+
+    if not muscle_moments_files2:
+        print(f'Muscle moments files found in {muscleAnalysis_dir2}: {muscle_moments_files2}')
+        return
+    
+    # load joint moments
+    jointMoments_df1 = utils.load_any_data_file(jointMomentsPath1)
+    jointMoments_df1 = utils.time_normalise_df(jointMoments_df1)
+
+    jointMoments_df2 = utils.load_any_data_file(jointMomentsPath2)
+    jointMoments_df2 = utils.time_normalise_df(jointMoments_df2)
+
+    for i,filePath in enumerate(muscle_moments_files1):
+
+        fileName1 = os.path.basename(filePath)
+        fileName2 = os.path.basename(muscle_moments_files2[i])
+        dof = fileName1.replace(MUSCLE_ANALYSIS_PREFIX, '').replace(MUSCLE_MOMENTS_PREFIX, '').replace('.sto', '')        
+
+        muscleMoment1_df = utils.load_any_data_file(muscle_moments_files1[i])
+        muscleMoment1_df = utils.time_normalise_df(muscleMoment1_df)
+
+        muscleMoment2_df = utils.load_any_data_file(muscle_moments_files2[i])
+        muscleMoment2_df = utils.time_normalise_df(muscleMoment2_df)
+
+        try:
+            jointMoment1_dof = jointMoments_df1[dof + '_moment']
+            jointMoment2_dof = jointMoments_df2[dof + '_moment']
+        except:
+            continue
+
+        try:
+            muscleMoment1_df,muscleMapping_dof = groups_muscles(muscleMoment1_df, muscleMapping)
+            muscleMoment2_df,_ = groups_muscles(muscleMoment2_df, muscleMapping)
+        except:
+            breakpoint()
+            continue
+
+        if muscleMoment1_df is None or muscleMoment2_df is None:
+            continue
+        
+        plt.figure(figsize=(12, 8))
+        
+        # Plot each muscle's moment
+        for muscleGroup in muscleMapping_dof.keys():
+            if 'time' not in muscleGroup.lower():
+                style = muscleMapping_dof[muscleGroup]['linestyle']
+                color = muscleMapping_dof[muscleGroup]['color']
+                # Plot for trial 1
+                plt.plot(muscleMoment1_df.index, muscleMoment1_df[muscleGroup], 
+                         label=muscleGroup,
+                         linestyle=style, color=color, linewidth=2.5)
+                # Plot for trial 2
+                plt.plot(muscleMoment2_df.index, muscleMoment2_df[muscleGroup],
+                         linestyle=style, color=color, linewidth=1, alpha=0.7)
+        
+        # Plot joint moments for comparison as a grey shade
+        # breakpoint()
+        plt.fill_between(jointMoment1_dof.index,
+                         jointMoment1_dof,
+                         color='grey', alpha=0.3, label='Net moment')
+        
+        plt.fill_between(jointMoment2_dof.index,
+                         jointMoment2_dof,
+                         color='brown', alpha=0.3, label='Net moment')
+
+        plt.xlabel('Time')
+        plt.ylabel('Muscle Moment (Nm)')
+        plt.title(f'Muscle Moments - {os.path.basename(fileName1)}')
+        # Adjust subplot parameters to make room for the legend
+        plt.subplots_adjust(right=0.75)
+        plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0.)
+        
+        # add to legend the brown shade 
+        
+        plt.grid(True)
+        plt.tight_layout()
+
+        outputFilePath = f'{savePath}\\{fileName1}_vs_{fileName2}.png'
+        os.makedirs(os.path.dirname(outputFilePath), exist_ok=True)
+        plt.savefig(outputFilePath)
+        print(f"Saved plot for file: {outputFilePath}")
+        plt.close()
+
+
+    
 if __name__ == '__main__':
    
     base_dir = paths.SIMULATION_DIR
     subject = 'Athlete_03_MRI'  # Replace with actual subject name
     session = '22_07_06'  # Replace with actual session name
-    trial = 'sq_75'  # Replace with actual trial name
+    trial_name = 'sq_75'  # Replace with actual trial name
     
     # create a trial instance
-    trial = paths.Trial(subject_name=subject, session_name=session, trial_name=trial)
+    trial = paths.Trial(subject_name=subject, session_name=session, trial_name=trial_name)
 
-    musicAnalysis_dir = trial.path + '\\' + trial.outputFiles['MA'].output
+    muscleAnalysis_dir = trial.path + '\\' + trial.outputFiles['MA'].output
     muscleForces_filepath = trial.outputFiles['FORCES_SO'].abspath()
     muscleMapping = paths.Settings().Muscle_Groups
     idOutput = trial.outputFiles['ID'].abspath()
 
     if True:
-        main(muscleAnalysis_dir=musicAnalysis_dir,
+        main(muscleAnalysis_dir=muscleAnalysis_dir,
              muscleForces_filepath=muscleForces_filepath)
     
     if True:
-        plot(muscleAnalysis_dir=musicAnalysis_dir, 
+        plot(muscleAnalysis_dir=muscleAnalysis_dir, 
              jointMomentsPath=idOutput,
              muscleMapping=muscleMapping)
+        
+    if True:
+        savePath = os.path.join(paths.RESULTS_DIR, subject, session, trial_name)
+        muscleAnalysis_dir2 = muscleAnalysis_dir.replace('_MRI','')
+        idOutput2 = idOutput.replace('_MRI','')
+        plot_2_trials(muscleAnalysis_dir1=muscleAnalysis_dir,
+                      jointMomentsPath1=idOutput,
+                      muscleAnalysis_dir2=muscleAnalysis_dir2,
+                      jointMomentsPath2=idOutput2,
+                      muscleMapping=muscleMapping,
+                      savePath=savePath)
