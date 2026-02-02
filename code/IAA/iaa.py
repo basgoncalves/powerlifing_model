73eba57d1c962f086afaeceab0ec78227d26e44a
Bas <basilio.goncalves7@gmail.com>
2026-02-02 21:20:47 +0100

    [CODE / RESULTS] improve trial comparison plots and add first results

--- code/IAA/iaa.py
+++ code/IAA/iaa.py
@@ -3,6 +3,9 @@ import sys
 print(sys.executable)
 import opensim as osim
 
+# Add parent directory to path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import utils
 current_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(current_dir)
 
@@ -17,6 +20,7 @@ def run_from_setup():
     CS_obj = CSo.updPropertyByIndex(1) # <objects>
     constraintR = CS_obj.updValueAsObject(0) #<RollingOnSurface ... >
     constraintL = CS_obj.updValueAsObject(1) #<RollingOnSurface ... >
+    
     propriedade = constraintR.updPropertyByName('socket_rolling_body')
     osim.PropertyHelper.setValueString('/bodyset/calcn_r', propriedade)
     propriedade = constraintR.updPropertyByName('socket_surface_body')
@@ -29,7 +33,10 @@ def run_from_setup():
 
     #analise3.verifyControlsStates() ## ok, i passes
 
-    analise3.run() # fails
+    try:
+        analise3.run() 
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     run_from_setup()
